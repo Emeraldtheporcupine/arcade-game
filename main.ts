df732a1cf7d481a0af3c68ebc2adce2f964ba92f
Emeraldtheporcupine <125104247+Emeraldtheporcupine@users.noreply.github.com>
2026-07-05 21:22:48 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -15,7 +15,8 @@ sprites.onOverlap(SpriteKind.Hazard, SpriteKind.Player, function (sprite, otherS
     }
     for (let value of list) {
         value.setPosition(otherSprite.x, otherSprite.y)
-        value.setBounceOnWall(true)
+        value.setFlag(SpriteFlag.GhostThroughWalls, true)
+        value.setFlag(SpriteFlag.AutoDestroy, true)
         animation.runImageAnimation(
         value,
         assets.animation`Ball`,
@@ -35,7 +36,7 @@ sprites.onOverlap(SpriteKind.Hazard, SpriteKind.Player, function (sprite, otherS
     music.play(music.createSoundEffect(WaveShape.Noise, 5000, 0, 159, 98, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     music.play(music.createSoundEffect(WaveShape.Noise, 5000, 0, 98, 35, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     music.play(music.createSoundEffect(WaveShape.Noise, 5000, 0, 17, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
-    timer.after(3000, function () {
+    timer.after(2000, function () {
         game.reset()
     })
 })
@@ -73,6 +74,7 @@ statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
     sprites.destroyAllSpritesOfKind(SpriteKind.Rok)
     sprites.destroyAllSpritesOfKind(SpriteKind.Overhead)
     sprites.destroyAllSpritesOfKind(SpriteKind.Hazard)
+    currentAttack = -1
     animation.stopAnimation(animation.AnimationTypes.All, Cloaker)
     music.stopAllSounds()
     animation.runImageAnimation(
@@ -83,7 +85,7 @@ statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
     )
 })
 sprites.onOverlap(SpriteKind.BOSS, SpriteKind.KillerRock, function (sprite, otherSprite) {
-    statusbar.value += -4
+    statusbar.value += -8
     sprites.destroy(otherSprite)
     animation.runImageAnimation(
     sprite,
@@ -121,6 +123,8 @@ sprites.onCreated(SpriteKind.Rok, function (sprite) {
         sprites.destroy(sprite)
     })
 })
+let Rock: Sprite = null
+let Lightning: Sprite = null
 let Warning: Sprite = null
 let KickDirection = 0
 let list: Sprite[] = []
@@ -129,11 +133,12 @@ let Cloaker: Sprite = null
 let Derpo: Sprite = null
 let doPattern = false
 let enemyDead = false
+let currentAttack = 0
 let isAttacking = false
 let currentAttackTime = 0
-let currentAttack = 0
+currentAttack = 0
 let runningTime = 0
-let CheckTime = 4000
+let CheckTime = 250
 enemyDead = false
 doPattern = false
 scene.setBackgroundColor(15)
@@ -167,9 +172,9 @@ assets.animation`CloakerIdle`,
 true
 )
 statusbar = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
-statusbar.value = 2
+statusbar.value = 100
 statusbar.attachToSprite(Cloaker)
-statusbar.setColor(7, 15)
+statusbar.setColor(7, 13)
 statusbar.setOffsetPadding(-25, -15)
 tiles.placeOnTile(Cloaker, tiles.getTileLocation(7, 0))
 Cloaker.vy = 50
@@ -177,20 +182,14 @@ let Center = sprites.create(assets.image`yay`, SpriteKind.Middle)
 tiles.placeOnTile(Center, tiles.getTileLocation(8, 10))
 Center.x += -8
 music.play(music.createSong(assets.song`Boss`), music.PlaybackMode.LoopingInBackground)
-game.onUpdate(function () {
-    runningTime += 2
-    info.setScore(runningTime)
-    if (isAttacking == false) {
-        if (runningTime - currentAttackTime > 250) {
-            if (true) {
-                Warning = sprites.create(assets.image`Uh Oh_`, SpriteKind.Overhead)
-                Warning.x = Derpo.x + KickDirection * 32
-                Warning.y = Derpo.y - 32
-            }
-            currentAttackTime = runningTime
-        }
-    }
-})
+scroller.setLayerImage(scroller.BackgroundLayer.Layer0, assets.image`layer0StarsNormal`)
+scroller.setCameraScrollingMultipliers(0.25, 0, scroller.BackgroundLayer.Layer0)
+scroller.setLayerImage(scroller.BackgroundLayer.Layer1, assets.image`layer1clouds`)
+scroller.setCameraScrollingMultipliers(0.45, 0, scroller.BackgroundLayer.Layer1)
+scroller.setBackgroundScrollOffset(0, -24, scroller.BackgroundLayer.Layer1)
+scroller.scrollBackgroundWithSpeed(-5, 0, scroller.BackgroundLayer.Layer1)
+scroller.setLayerImage(scroller.BackgroundLayer.Layer2, assets.image`layer2ground`)
+scroller.setCameraScrollingMultipliers(0.5, 0, scroller.BackgroundLayer.Layer2)
 game.onUpdate(function () {
     if (controller.right.isPressed()) {
         KickDirection = 1
@@ -200,6 +199,11 @@ game.onUpdate(function () {
         Derpo.vx += -2
     } else {
         Derpo.vx += Derpo.vx * -0.15
+    }
+    if (Derpo.vx > 0 && controller.left.isPressed()) {
+        Derpo.vx += -2
+    } else if (Derpo.vx < 0 && controller.right.isPressed()) {
+        Derpo.vx += 2
     }
     if (Derpo.vx > 76) {
         Derpo.vx = 76
@@ -220,8 +224,70 @@ game.onUpdate(function () {
         Cloaker.vy = 0
     }
 })
-game.onUpdateInterval(CheckTime, function () {
+game.onUpdate(function () {
+    runningTime += 2
     if (enemyDead == false) {
-        CheckPatterns()
+        if (runningTime - currentAttackTime > CheckTime) {
+            if (currentAttack == 0) {
+                Warning = sprites.create(assets.image`Uh Oh_`, SpriteKind.Overhead)
+                Warning.x = Derpo.x + KickDirection * 32
+                Warning.y = Derpo.y - 32
+                CheckTime = 40
+                currentAttack = 1
+            } else if (currentAttack == 1) {
+                sprites.destroy(Warning)
+                scene.setBackgroundColor(9)
+                Lightning = sprites.create(assets.image`yay`, SpriteKind.Hazard)
+                Lightning.x = Warning.x
+                Lightning.y = Warning.y - 68
+                animation.runImageAnimation(
+                Lightning,
+                assets.animation`Lightning`,
+                50,
+                true
+                )
+                CheckTime = 150
+                currentAttack = 2
+                timer.after(150, function () {
+                    scene.setBackgroundColor(15)
+                    sprites.destroy(Lightning)
+                })
+            } else if (currentAttack == 2) {
+                Warning = sprites.create(assets.image`Uh Oh_`, SpriteKind.Overhead)
+                Warning.x = Derpo.x
+                Warning.y = Derpo.y - 32
+                CheckTime = 50
+                currentAttack = 3
+            } else if (currentAttack == 3) {
+                sprites.destroy(Warning)
+                scene.setBackgroundColor(9)
+                Lightning = sprites.create(assets.image`yay`, SpriteKind.Hazard)
+                Lightning.x = Warning.x
+                Lightning.y = Warning.y - 68
+                animation.runImageAnimation(
+                Lightning,
+                assets.animation`Lightning`,
+                50,
+                true
+                )
+                CheckTime = 150
+                currentAttack = 4
+                timer.after(150, function () {
+                    scene.setBackgroundColor(15)
+                    sprites.destroy(Lightning)
+                })
+            } else if (currentAttack == 4) {
+                Rock = sprites.create(assets.image`Boulder`, SpriteKind.Rok)
+                Rock.x = Cloaker.x
+                Rock.y = Cloaker.y
+                Rock.ay = 400
+                Rock.vy = -150
+                Rock.vx = KickDirection / 4
+                Rock.setFlag(SpriteFlag.AutoDestroy, true)
+                CheckTime = 150
+                currentAttack = 0
+            }
+            currentAttackTime = runningTime
+        }
     }
 })
